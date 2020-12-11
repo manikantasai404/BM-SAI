828e50817a5c28b776fcdd4dba3e2b54616b44be
manikantasai404 <56576399+manikantasai404@users.noreply.github.com>
2020-12-11 17:03:25 +0530

    Leverage charges error fix

--- src.js
+++ src.js
@@ -8,7 +8,7 @@ getPrice = function () {
     numVal1 = Number(document.getElementById("price").value);
     var numVal2 = Number(document.getElementById("discount").value) / 100;
     totalValue = numVal1 - (numVal1 - (numVal1 * numVal2));
-    document.getElementById("total").value = totalValue.toFixed(2)+10;
+    document.getElementById("total").value = (totalValue + 10).toFixed(2);
     lossValue = totalValue * 2
     document.getElementById("loss").value = lossValue.toFixed(2);
 }
