3a0338e97238a123c9d6187e7cc80b0305f27e16
manikantasai404 <56576399+manikantasai404@users.noreply.github.com>
2020-12-11 17:00:28 +0530

    Charges for leverage added

--- src.js
+++ src.js
@@ -7,8 +7,8 @@ var totalShares;
 getPrice = function () {
     numVal1 = Number(document.getElementById("price").value);
     var numVal2 = Number(document.getElementById("discount").value) / 100;
-    totalValue = (numVal1 - (numVal1 - (numVal1 * numVal2)))+10;
-    document.getElementById("total").value = totalValue.toFixed(2);
+    totalValue = numVal1 - (numVal1 - (numVal1 * numVal2));
+    document.getElementById("total").value = totalValue.toFixed(2)+10;
     lossValue = totalValue * 2
     document.getElementById("loss").value = lossValue.toFixed(2);
 }
